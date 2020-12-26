43ed0cd17f3bf2586a89eec6e54683595f16e441
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2020-12-26 07:01:06 +0000

    libceph: request a new osdmap if lingering request maps to no osd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,7 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be refer
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// co
