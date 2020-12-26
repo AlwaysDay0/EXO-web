495f98750d8e4fb7394c170cd461da0c03acaf95
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2020-12-26 17:13:56 +0000

    update gitconfig

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,6 @@
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// co
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/
