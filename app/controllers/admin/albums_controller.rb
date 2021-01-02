703efa7f8196416123646b3184330f70f7b028a9
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-01-02 22:55:47 +0000

    Version 1.3.1

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -1,1 +1,5 @@
-class Admin::AlbumsCont
+class Admin::AlbumsController < ApplicationController
+  before_action :set_albums, only: [:show, :edit, :update, :destroy, :add_record, :save_record]
+
+  def index
+    @albums
