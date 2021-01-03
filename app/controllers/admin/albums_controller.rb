dfba964d85d66922e8d35da3a0c988c24cafdbef
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-01-03 17:57:50 +0000

    some layout adjustments

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -2,4 +2,20 @@ class Admin::AlbumsController < ApplicationController
   before_action :set_albums, only: [:show, :edit, :update, :destroy, :add_record, :save_record]
 
   def index
-    @albums
+    @albums = Album.all
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def edit
+  end
+  
+  def show
+  end
+
+  def add_record
+  end
+
+  def save_reco
