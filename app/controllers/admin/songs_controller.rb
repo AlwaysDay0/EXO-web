91f9710f3a7ac206b9b584d42b5a1b6a98a2af1b
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-01-08 21:45:36 +0000

    Release 0.5.1

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,2 +1,16 @@
 class Admin::SongsController < ApplicationController
-  before_action :set_song
+  before_action :set_songs, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+  end
+  
+  def show
+  end
