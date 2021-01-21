7b7cdf1d1503151fda1a51df6a7f55fc9f428dbf
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-01-21 17:48:04 +0000

    adding badges

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,5 @@
 class Artist < ApplicationRecord
-  has_and_belongs_to_many :songs, join_table: 'ass
+  has_and_belongs_to_many :songs, join_table: 'associations', dependent: :delete_all
+  has_and_belongs_to_many :albums, join_table: 'associations', dependent: :delete_all
+  
+  validates :nam
