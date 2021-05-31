f170d9d3d7d4942d0cd199677aded9aa81298a1b
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-05-31 11:46:12 +0000

    create future branch which bases on SuperSocket 1.5

--- config/application.rb
+++ config/application.rb
@@ -24,4 +24,8 @@ module AppleMusic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are aut
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators
