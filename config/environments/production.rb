8bdcd243100d9a4a9cf1ab53c55269887e7abbfe
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-08-01 13:09:19 +0000

    chore(package): update coveralls to version 2.11.8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,2 +1,7 @@
 Rails.application.configure do
-  # Settings 
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  
