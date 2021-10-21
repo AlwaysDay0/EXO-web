88d66cbb4d0abbb7f0264907b23d2b884fd530cc
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-21 19:47:53 +0000

    More details.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -19,4 +19,6 @@
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
-# Report 
+# Report CSP violations to a specified URI
+# For further information see the following documentation:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Se
