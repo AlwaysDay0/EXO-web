7f7b6b004a51aeadeece4d6c8a883ec3a6facb7a
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-23 21:32:51 +0000

    removed printlns

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-# Sp
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :marshal, and :hybrid.
+Rails.application.config.action_dis
