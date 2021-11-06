57602e55a5002ba7416e74c4d42dc2c1790cbe66
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-11-06 10:41:07 +0000

    Refactor test cases

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,1 +1,6 @@
-# Be sure to restart your server when you 
+# Be sure to restart your server when you modify this file.
+
+# This file contains settings for ActionController::ParamsWrapper which
+# is enabled by default.
+
+# Enable parameter wrapping for JS
