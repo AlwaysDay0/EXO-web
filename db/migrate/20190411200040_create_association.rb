112738ffaf51a23285ce647f24d62c82c5f460c1
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2022-03-14 19:28:29 +0000

    FH fixes

--- db/migrate/20190411200040_create_association.rb
+++ db/migrate/20190411200040_create_association.rb
@@ -3,4 +3,7 @@ class CreateAssociation < ActiveRecord::Migration[5.2]
     create_table :associations do |t|
       t.references :album, foreign_key: true
       t.references :song, foreign_key: true
-      t.references :artist, f
+      t.references :artist, foreign_key: true
+    end
+  end
+end
