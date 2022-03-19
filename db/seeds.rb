115b2b89f8954a1be0d76135b9de1572ec5a0a4f
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2022-03-19 09:05:04 +0000

    Updated news item

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+
+artist1 = Artist.create(name: "Nujabes", age: "31")
+artist2 = Artist.create(name: "Frank Sinatra", age: "43")
+artist3 = Artist.create(name: "Maluma", age: "28")
+artist4 = Artist.create(name: "Taylor Swift", age: "28")
+
+song1 = Song.create(title: "Imaginary Folklore", duration: "3:50", rating: 5, progress: 0)
+song2 = Song.create(title: "Space dream", duration: "4:50", rating: 5, progress: 0)
+song3 = Song.create(title: "Shake it off", duration: "3:50", rating: 5, progress: 0)
+song4 = Song.create(title: "Felices los 4", duration: "3:50", rating: 5, progress: 0)
+
+album = Album.create(title: "Album Nujabes", rating: 3)
+album.songs << song1
+album.artists << artist1
+
+album2 = Album.create(title: "Album Sinatra", rating: 3)
+album2.songs << song2
+album2.artists << artist2
+
+album3 = Album.create(title: "Album Maluma", rating: 3)
+album3.songs << song3
+album3.artists << artist3
+
+album4 = Album.create(title: "Album Taylor", rating: 3)
+album4.songs << song4
+album4.artists << artist4
