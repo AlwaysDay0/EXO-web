7b164762820aebbb476722fb775dad6ff16963c3
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2022-10-20 16:31:56 +0000

    #17

--- spec/controllers/api/songs_controller_spec.rb
+++ spec/controllers/api/songs_controller_spec.rb
@@ -43,4 +43,9 @@ describe Api::SongsController do
   describe 'GET artists' do
     it 'returns http status ok' do
       get :artists, params: { song_id: @song }
-      
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'render the correct artists' do
+        @song.artists << Artist.create(name: "MAluma")
+        get :artists
