171c25317adb654482dc5092374e1e73a05457a3
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-18 09:57:48 +0000

    Finish

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,2 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'sp
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the envi
