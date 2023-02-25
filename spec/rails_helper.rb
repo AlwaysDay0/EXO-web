8ab76cf5c4f77be1f5f0e99ff797e47c04e6c299
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-25 12:08:26 +0000

    removed test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,4 +11,6 @@ require 'rspec/rails'
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matchin
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-lo
