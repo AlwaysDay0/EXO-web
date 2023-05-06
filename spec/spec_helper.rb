2ca6466b49137e8da1ed0a34096912002c8ccf19
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-05-06 17:42:41 +0000

    Fix off-by-one error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,4 +50,8 @@ RSpec.configure do |config|
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
-  # aliases for `it`, `descri
+  # aliases for `it`, `describe`, and `context` that include `:focus`
+  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
+  config.filter_run_when_matching :focus
+
+  # Allo
