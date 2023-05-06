6f64d766ffb60f78bf8a2aee03e60c1482c693fd
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-05-06 21:28:45 +0000

    update scala version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,4 +56,7 @@ RSpec.configure do |config|
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
-  # you configure you
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # Limits the avai
