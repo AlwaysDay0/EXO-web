0876bcebc45c321a8a228b0a5eca5e61522edd61
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-05-23 15:30:17 +0000

    Run update-remote.sh on mysql

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,4 +78,8 @@ RSpec.configure do |config|
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
-  # particular
+  # particularly slow.
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, y
