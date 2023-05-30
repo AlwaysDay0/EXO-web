40e151d7a557ba5b2193e8fb02972df14a67c0cf
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-05-30 09:29:57 +0000

    Missing includes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,4 +88,9 @@ RSpec.configure do |config|
   config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically repro
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+=end
+end
